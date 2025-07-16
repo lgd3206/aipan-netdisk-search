df19abee864c39af031238017bdca9f693a0bc5e
lgd3206 <329938313@qq.com>
2025-07-16 21:09:12 +0800

    Create channels.ts

---
+++ lib/channels.ts
@@ -0,0 +1,18 @@
+// lib/channels.ts
+export const channels = [
+  {
+    name: "115影视资源分",
+    key: "guaguale115",
+    apiUrl: "https://api.pansou.com/search?key=电影", // 这里填真实API地址
+    enabled: true,
+    type: "api"
+  },
+  {
+    name: "阿里云盘发布",
+    key: "shareAliyun",
+    apiUrl: "https://api.pansoso.com/search?keyword=电影",
+    enabled: true,
+    type: "api"
+  },
+  // ... 你可以继续添加其它频道
+];
